c6974ef8c6c7ce41981203c9a221b071780fc458
João Vitor Bueno <joao.gomes1001@gmail.com>
2022-08-10 15:15:44 -0300

    English version creation

--- circumference_calculator.py
+++ circumference_calculator.py
@@ -1,53 +1,53 @@
 #Calculadora de Circulos#
 
 def welcome():
-    print("E aí Bora calcular?")
+    print("So let's calculate?")
 
 welcome()
 
 def Calculate():
     π=3.1415
-    operação = input('''Escolha seu calculo utlizando um dos Codigos abaixo:
-ec para Obter o Raio
-d para Obter o Diâmetro
-cc para Obter o Comprimentro da Circunfêrencia
-ac para Obter a Área da Circunferência
+    opr= input('''Choose your calculation using one of the codes below:
+ec to Get the Radius
+d to Get the Diameter
+cc to Get the Length of the Circumference
+ac to Get the Area of the Circumference
 ''')
 
-    if operação == "ec":
-        Numero_1 = int(input("Insira o Diâmetro:"))
-        print("D=2.r")
-        print("{}=2.R=".format(Numero_1))
-        print("R={}/2=".format(Numero_1))
-        print("R = ",Numero_1/2)
-    elif operação == "d":
-        Numero_1 = int(input("Insira o Raio:"))
-        print("D=2.r")
-        print("D={}.2".format(Numero_1))
-        print("D = ",Numero_1*2)
-    elif operação == "cc":
-        Numero_1 = int(input("Insira o Raio:"))
-        print("C=2.π.r")
-        print("C = 2(3,14).{}".format(Numero_1))
-        print("C = 6,28.{}".format(Numero_1))
-        print("C = ",2*π*Numero_1)
-    elif operação == "ac":
-        Numero_1 = int(input("Insira o Raio:"))
-        print("A=π.r²")
-        print("A = 3,14.{}²".format(Numero_1))
-        print("A = 3,14.",(Numero_1**2))
-        print("A = ",(π*Numero_1**2))
+    if opr == "ec":
+        num1 = int(input("Enter Diameter:"))
+        print("D = 2.r")
+        print("{} = 2.R".format(num1))
+        print("R = {}/2".format(num1))
+        print("R = ",num1/2)
+    elif opr == "d":
+        num1 = int(input("Enter Radius:"))
+        print("D = 2.r")
+        print("D = {}.2".format(num1))
+        print("D = ",num1*2)
+    elif opr == "cc":
+        num1 = int(input("Enter Radius:"))
+        print("C = 2.π.r")
+        print("C = 2(3,14).{}".format(num1))
+        print("C = 6,28.{}".format(num1))
+        print("C = ",2*π*num1)
+    elif opr == "ac":
+        num1 = int(input("Enter Radius:"))
+        print("A = π.r²")
+        print("A = 3,14.{}²".format(num1))
+        print("A = 3,14.",(num1**2))
+        print("A = ",(π*num1**2))
     else:
-        print("Codigo Invalido. Tente Novamente")
+        print("Invalid code. Try again")
     again()
 
 def again():
-    calc_again = input("Gostaria de fazer outro cálculo?")
+    calc_again = input("Would you like to do another calculation?(y/n)")
 
     if calc_again.upper() =='Y':
         Calculate()
     elif calc_again.upper()=='N':
-        print("Até Breve :)")
+        print("See you later :)")
     else:
         again()
 
